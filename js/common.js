241cd0c2124b9e4e2f7e0fdc508fb2ecbeca262a
DESKTOP-3KSGD30\admin <Aaron.xiong@800teleservices.com>
2024-08-02 16:42:07 +0800

    1

--- js/common.js
+++ js/common.js
@@ -9,19 +9,21 @@ $(function () {
     }
     $(this).addClass("active").siblings().removeClass("active");
     const index = $(this).index();
-    const pageWidth = window.innerWidth;
-    if (pageWidth > 768) {
-      // web端
-      const sceneWbeHtml = `          
-      <div class="d-flex justify-content-between">
-            <div class="service-item p-2 border rounded">
+    const sceneWbeHtml = ` 
+      <div class="d-flex service-box justify-content-between">
+            <div class="service-item p-md-2  mb-3 mb-md-0 ">
               <div class="position-relative">
-                <img src="./images/service_pic001.png" class="img-fluid" />
-                <div class="service-item-cont">
-                  <a href="./complianceDetails.html" class="text-white">
-                    <h3 class="mb-4 font-24">${index}二手车交易</h3>
+                <div class="d-none d-md-block">
+                  <img src="./images/service_pic001.png" class="img-fluid" />
+                </div>
+                <div class="d-block d-md-none">
+                  <img src="./images/serviceM_pic001.png" class="img-fluid" />
+                </div>
+                <div class="service-item-cont px-4 py-1">
+                  <a href="./solutionsDetails.html">
+                    <h3 class="mb-2 mb-md-4 font-24">${index}二手车交易</h3>
                     <div>
-                      <p class="font-18 line-height-2">
+                      <p class="font-18 mb-0">
                         推动二手车行业 合规的收集和使 用汽车数据， 促进行业发展
                       </p>
                     </div>
@@ -29,14 +31,19 @@ $(function () {
                 </div>
               </div>
             </div>
-            <div class="service-item p-2 border rounded">
+            <div class="service-item p-md-2  mb-3 mb-md-0 service-item-two">
               <div class="position-relative">
-                <img src="./images/service_pic002.png" class="img-fluid" />
-                <div class="service-item-cont">
-                  <a href="./complianceDetails.html" class="text-white">
-                    <h3 class="mb-4 font-24">${index}新能源汽车</h3>
+                <div  class="d-none d-md-block">
+                  <img src="./images/service_pic002.png" class="img-fluid" />
+                </div>
+                <div  class="d-block d-md-none">
+                  <img src="./images/serviceM_pic002.png" class="img-fluid" />
+                </div>
+                <div class="service-item-cont px-4 py-1 px-md-0 py-md-0">
+                  <a href="./solutionsDetails.html">
+                    <h3 class="mb-2 mb-md-4 font-24">${index}新能源汽车</h3>
                     <div>
-                      <p class="font-18 line-height-2">
+                      <p class="font-18 mb-0">
                         赋能新能源车企 助力车企在合规 的前提下收集和
                         处理汽车数据
                       </p>
@@ -45,14 +52,19 @@ $(function () {
                 </div>
               </div>
             </div>
-            <div class="service-item p-2 border rounded">
+            <div class="service-item p-md-2  mb-3 mb-md-0">
               <div class="position-relative">
-                <img src="./images/service_pic003.png" class="img-fluid" />
-                <div class="service-item-cont">
-                  <a href="./complianceDetails.html" class="text-white">
-                    <h3 class="mb-4">${index}车联网</h3>
+                <div  class="d-none d-md-block">
+                  <img src="./images/service_pic003.png" class="img-fluid" />
+                </div>
+                <div  class="d-block d-md-none">
+                  <img src="./images/serviceM_pic003.png" class="img-fluid" />
+                </div>
+                <div class="service-item-cont px-4 py-1 px-md-0 py-md-0">
+                  <a href="./solutionsDetails.html">
+                    <h3 class="mb-2 mb-md-4 font-24">${index}车联网</h3>
                     <div>
-                      <p class="h5">
+                      <p class="font-18 mb-0">
                         促进车联网企业合规 的收集和使用汽车相 关的数据，降低企业
                         经营风险
                       </p>
@@ -62,72 +74,7 @@ $(function () {
               </div>
             </div>
           </div>`;
-      $("#serviceWeb").html(sceneWbeHtml);
-    } else {
-      const servicePhoneHtml = `
-        <div class="d-block d-md-none">
-          <div>
-            <div class="service-item mb-2">
-              <div class="position-relative">
-                <img src="./images/serviceM_pic001.png" class="img-fluid" />
-                <div class="service-item-cont d-flex align-items-center px-4 py-2">
-                  <div>
-                    <a href="./complianceDetails.html" class="text-dark">
-                      <h3 class="mb-3">${index}二手车交易</h3>
-                      <div>
-                        <span>
-                          推动二手车行业 合规的收集和使 用汽车数据，
-                          促进行业发展
-                        </span>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="service-item mb-2">
-              <div class="position-relative">
-                <img src="./images/serviceM_pic002.png" class="img-fluid" />
-                <div class="service-item-cont d-flex align-items-center px-4 py-2">
-                  <div>
-                    <a href="./complianceDetails.html" class="text-dark">
-                      <h3 class="mb-3">${index}新能源汽车</h3>
-                      <div>
-                        <span>
-                          赋能新能源车企 助力车企在合规 的前提下收集和
-                          处理汽车数据
-                        </span>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="service-item mb-2">
-              <div class="position-relative">
-                <img src="./images/serviceM_pic003.png" class="img-fluid" />
-                <div class="service-item-cont d-flex align-items-center px-4 py-2">
-                  <div>
-                    <a href="./complianceDetails.html" class="text-dark">
-                      <h3 class="mb-3">${index}车联网</h3>
-                      <div>
-                        <span>
-                          促进车联网企业合规 的收集和使用汽车相
-                          关的数据，降低企业 经营风险
-                        </span>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      `;
-      // web端
-      $("#servicePhone").html(servicePhoneHtml);
-      return;
-    }
+    $("#serviceCont").html(sceneWbeHtml);
   });
   $("#applyBtn").click(function () {
     $("#popTrial").show();
@@ -150,12 +97,12 @@ $(function () {
   let countdown; // 倒计时计时器变量
   $("#btn-countdown").click(function () {
     let seconds = 60; // 这里可以根据需要设置倒计时的秒数
-    const countdownBtn = document.getElementById('btn-countdown');
+    const countdownBtn = document.getElementById("btn-countdown");
     // 禁用按钮防止重复点击
     countdownBtn.disabled = true;
     // 显示初始倒计时
     countdownBtn.textContent = seconds;
- 
+
     // 开始倒计时
     countdown = setInterval(() => {
       seconds--;
